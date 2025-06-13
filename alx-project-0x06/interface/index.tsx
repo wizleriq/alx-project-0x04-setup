03e765c9b451d68814b85e1dd1b64800eadbcd37
Ndata <wisdomndata@gmail.com>
2025-06-13 15:45:33 +0100

    New Directory

---
+++ alx-project-0x06/interface/index.tsx
@@ -0,0 +1,10 @@
+export interface ButtonProps {
+  buttonLabel: string;
+  buttonSize?: string;
+  buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green';
+  action?: () => void;
+}
+
+// export interface LayoutProps {
+//   children: ReactNode;
+// }
